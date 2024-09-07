33891be2c83f78951e6e69171869c30d2307bfe0
Thrinath17 <thrinath.ikkurthi@gmail.com>
2024-09-06 19:02:35 -0500

    Updated the app name

--- ui/src/components/AddTaskForm.js
+++ ui/src/components/AddTaskForm.js
@@ -26,7 +26,7 @@ export const AddTaskForm = ({ fetchTasks }) => {
   return (
     <div>
       <Typography align="center" variant="h2" paddingTop={2} paddingBottom={2}>
-        My Task List
+        To-do List
       </Typography>
       <div className="addTaskForm">
         <TextField
